c0983ae631c41be7e95ea9f84c5f8eab439fde00
huangyiming9228 <764520351@qq.com>
2019-01-28 21:53:12 +0800

    添加后台页面依赖

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -6,10 +6,12 @@ use think\Db;
 
 class Index extends Controller
 {
+  // 渲染首页
   public function index(){
     return $this->fetch('index');
   }
 
+  // 渲染欢迎页
   public function welcome() {
     return $this->fetch('welcome');
   }
