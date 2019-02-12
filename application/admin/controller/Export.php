c40c63b13cc06058bef9310ab3f1be4f53609586
Jarrett Huang <jarretth@synnex.com>
2019-02-12 11:22:55 +0800

    引入phpspreadsheet

--- application/admin/controller/Export.php
+++ application/admin/controller/Export.php
@@ -4,6 +4,8 @@ namespace app\admin\controller;
 use think\Controller;
 use think\Db;
 use think\Session;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class Export extends Controller
 {
@@ -20,6 +22,19 @@ class Export extends Controller
     if ($status != '99') $conditions['status'] = $status;
     $conditions['visit_time'] = ['between time', [$start_time, $end_time]];
     return Db::table('reservation_list')->where($conditions)->order('visit_time', 'asc')->select();
+  }
+
+  public function test() {
+    $spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
+    $sheet->setCellValue('A1', 'Hello World !');
+    $filename = 'helloworld.xlsx';
+
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    header('Content-Disposition: attachment;filename="'.$filename.'"');
+    header('Cache-Control: max-age=0');
+    $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+    $writer->save('php://output');
   }
 
 }
