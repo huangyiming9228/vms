542ec01796435614f352e69347d72afdcec60778
huangyiming9228 <764520351@qq.com>
2019-03-17 18:52:11 +0800

    修改pc端菜单授权

--- application/index/controller/Pc.php
+++ application/index/controller/Pc.php
@@ -9,4 +9,15 @@ class Pc extends Controller {
   public function index() {
     $this->redirect('http://clgl.swpu.edu.cn/public/pc');
   }
+
+  public function get_menus($emp_no) {
+    $emp_level = Db::table('employee_list')->where('emp_no', $emp_no)->value('emp_level');
+    $menus = Db::table('pc_menu_seek')->where('level', $emp_level)->select();
+    $menus_info = [];
+    foreach ($menus as $key => $value) {
+      $res = Db::table('pc_menus')->where('id', $value['menu_id'])->find();
+      array_push($menus_info, $res);
+    }
+    return $menus_info;
+  }
 }
